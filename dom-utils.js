37c989dc26c04289d50d2d128e9e04ce8030e588
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2025-10-27 12:37:09 -0500

    Update dom-utils.js

--- dom-utils.js
+++ dom-utils.js
@@ -1,100 +1,102 @@
-
-
-const updateAttribute = (element, key, value) =>{
-  try{
-    const oldValue = element.getAttribute(String(key));
-    if(oldValue == value)return;
-    return element.setAttribute(String(key),String(value));
-  }catch(e){
-    console.warn(e,element,key,value);
-  }
+const NodeListFrom = (...args) => {
+    const nodes = document.createElement('NodeList').childNodes;
+    Object.defineProperty(nodes, 'length', {
+        value: args,
+        configurable: true,
+        writable: true
+    });
+    Object.setPrototypeOf(args, NodeList.prototype);
+    Object.setPrototypeOf(nodes, args);
+    return nodes;
 };
-
-const querySelector = (...args)=>{
-  let [elem,selectors] = args;
-  try{
-   if(!elem?.querySelector){
-      [selectors,elem] = [elem,document];
+const updateAttribute = (element, key, value) => {
+    try {
+        const oldValue = element.getAttribute(String(key));
+        if (oldValue == value) return;
+        return element.setAttribute(String(key), String(value));
+    } catch (e) {
+        console.warn(e, element, key, value);
     }
-    return elem.querySelector(String(selectors));
-  }catch(e){
-    console.warn(e,...args);
-  }
 };
-
-const querySelectorAll = (...args)=>{
-  let [elem,selectors] = args;
-  try{
-    if(!elem?.querySelectorAll){
-      [selectors,elem] = [elem,document];
+const querySelector = (...args) => {
+    let [elem, selectors] = args;
+    try {
+        if (!elem?.querySelector) {
+            [selectors, elem] = [elem, document];
+        }
+        return elem.querySelector(String(selectors));
+    } catch (e) {
+        console.warn(e, ...args);
     }
-    return elem.querySelectorAll(String(selectors));
-  }catch(e){
-    console.warn(e,...args);
-  }
-  return document.createElement('NodeList').childNodes;
 };
-
-const getElementById = (...args)=>{
-  let [elem,id] = args;
-  try{
-    if(!elem?.getElementById){
-      [id,elem] = [elem,document];
+const querySelectorAll = (...args) => {
+    let [elem, selectors] = args;
+    try {
+        if (!elem?.querySelectorAll) {
+            [selectors, elem] = [elem, document];
+        }
+        return elem.querySelectorAll(String(selectors));
+    } catch (e) {
+        console.warn(e, ...args);
     }
-    return elem.getElementById(String(id));
-  }catch(e){
-    console.warn(e,...args);
-  }
+    return document.createElement('NodeList').childNodes;
 };
-
-const getElementsByTagName = (...args)=>{
-  let [elem,tag] = args;
-  try{
-    if(!elem?.getElementsByTagName){
-      [tag,elem] = [elem,document];
+const getElementById = (...args) => {
+    let [elem, id] = args;
+    try {
+        if (!elem?.getElementById) {
+            [id, elem] = [elem, document];
+        }
+        return elem.getElementById(String(id));
+    } catch (e) {
+        console.warn(e, ...args);
     }
-    return elem.getElementsByTagName(String(tag));
-  }catch(e){
-    console.warn(e,...args);
-  }
-  return document.createElement('HTMLCollection').children;
 };
-
-const getElementsByTagNameNS = (...args)=>{
-  let [elem,tag] = args;
-  try{
-    if(!elem?.getElementsByTagNameNS){
-      [tag,elem] = [elem,document];
+const getElementsByTagName = (...args) => {
+    let [elem, tag] = args;
+    try {
+        if (!elem?.getElementsByTagName) {
+            [tag, elem] = [elem, document];
+        }
+        return elem.getElementsByTagName(String(tag));
+    } catch (e) {
+        console.warn(e, ...args);
     }
-    return elem.getElementsByTagNameNS(String(tag));
-  }catch(e){
-    console.warn(e,...args);
-  }
-  return document.createElement('HTMLCollection').children;
+    return document.createElement('HTMLCollection').children;
 };
-
-const getElementsByName = (...args)=>{
-  let [elem,name] = args;
-  try{
-    if(!elem?.getElementsByName){
-      [name,elem] = [elem,document];
+const getElementsByTagNameNS = (...args) => {
+    let [elem, tag] = args;
+    try {
+        if (!elem?.getElementsByTagNameNS) {
+            [tag, elem] = [elem, document];
+        }
+        return elem.getElementsByTagNameNS(String(tag));
+    } catch (e) {
+        console.warn(e, ...args);
     }
-    return elem.getElementsByName(String(name));
-  }catch(e){
-    console.warn(e,...args);
-  }
-  return document.createElement('HTMLCollection').children;
+    return document.createElement('HTMLCollection').children;
 };
-
-const getElementsByClassName = (...args)=>{
-  let [elem,name] = args;
-  try{
-    if(!elem?.getElementsByClassName){
-      [name,elem] = [elem,document];
+const getElementsByName = (...args) => {
+    let [elem, name] = args;
+    try {
+        if (!elem?.getElementsByName) {
+            [name, elem] = [elem, document];
+        }
+        return elem.getElementsByName(String(name));
+    } catch (e) {
+        console.warn(e, ...args);
     }
-    return elem.getElementsByClassName(String(name));
-  }catch(e){
-    console.warn(e,...args);
-  }
-  return document.createElement('HTMLCollection').children;
+    return document.createElement('HTMLCollection').children;
+};
+const getElementsByClassName = (...args) => {
+    let [elem, name] = args;
+    try {
+        if (!elem?.getElementsByClassName) {
+            [name, elem] = [elem, document];
+        }
+        return elem.getElementsByClassName(String(name));
+    } catch (e) {
+        console.warn(e, ...args);
+    }
+    return document.createElement('HTMLCollection').children;
 };
