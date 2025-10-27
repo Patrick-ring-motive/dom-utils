5abb47320ba93e00144ddee04b5e4e99b1be474b
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2025-10-27 12:27:45 -0500

    Update dom-utils.js

--- dom-utils.js
+++ dom-utils.js
@@ -10,3 +10,40 @@ const updateAttribute = (element, key, value) =>{
   }
 };
 
+const querySelector = (...args)=>{
+  let [elem,selectors] = args;
+  try{
+   if(!elem?.querySelector){
+      [selectors,elem] = [elem,document];
+    }
+    return elem.querySelector(String(selectors));
+  }catch(e){
+    console.warn(e,...args);
+  }
+};
+
+const querySelectorAll = (...args)=>{
+  let [elem,selectors] = args;
+  try{
+    if(!elem?.querySelectorAll){
+      [selectors,elem] = [elem,document];
+    }
+    return elem.querySelectorAll(String(selectors));
+  }catch(e){
+    console.warn(e,...args);
+  }
+  return document.createElement('NodeList').childNodes;
+};
+
+const getElementsByTagName = (...args)=>{
+  let [elem,tag] = args;
+  try{
+    if(!elem?.getElementsByTagName){
+      [tag,elem] = [elem,document];
+    }
+    return elem.getElementsByTagName(String(tag));
+  }catch(e){
+    console.warn(e,...args);
+  }
+  return document.createElement('HTMLCollection').children;
+};
